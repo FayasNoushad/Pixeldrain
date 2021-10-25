7412a72bc586ff14028817657ab283745330eb5f
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-25 10:02:48 +0530

    Added thumbnail

--- pixeldrain.py
+++ pixeldrain.py
@@ -35,3 +35,24 @@ def info(file_id):
     """
     info = requests.get(f"https://pixeldrain.com/api/file/{file_id}/info")
     return info
+
+
+def thumbnail(file_id, width="", height=""):
+    """
+    Returns a PNG thumbnail image representing the file.
+    The thumbnail image will be 128x128 px by default.
+    You can specify the width and height with parameters in the URL.
+    The width and height parameters need to be a multiple of 16.
+    So the allowed values are 16, 32, 48, 64, 80, 96, 112 and 128.
+    If a thumbnail cannot be generated for the file you will be redirected to a mime type image of 128x128 px.
+    
+    thumbnail(file_id, width, height)
+    width and height is optional
+    """
+    api = f"https://pixeldrain.com/api/file/{file_id}/thumbnail"
+    api += "?" if width or height else ""
+    api += "width=" + width if width else ""
+    api += "&" if width and height else ""
+    api += "height=" + height if height else ""
+    thumbnail = requests.get(api)
+    return thumbnail
